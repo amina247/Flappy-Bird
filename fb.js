07cb6adb251554421b81c0750d997582b066577a
amina247 <aminakhedr7@gmail.com>
2023-03-08 11:40:30 +0100

    first commit

---
+++ fb.js
@@ -0,0 +1,56 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var player = document.getElementById("player");
+var jumping = 0;
+var counter = 0;
+
+
+
+//making the pipes in random postions
+hole.addEventListener('animationiteration', () => {
+	let random = Math.random()*3;
+	let top = (random*100)+150;
+	hole.style.top = -(top) + "px";
+	counter++;
+});
+
+
+//Gravity stimulation
+setInterval(function(){
+	var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+
+	//change top if we are not currently jumping or else ball would be moving up and down
+	if(jumping===0){
+		charachter.style.top = (charachterTop+3)+ "px"
+	}
+	var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+	if(playerTop>480){
+		alert("Game over. Score: "+counter);
+		player.style.top = 100 + "px";
+		counter = 0;
+	}
+},10)
+
+
+//make the player jumps
+function jump(){
+	jumping = 1;
+	let jumpCount = 0;
+	var jumpInterval = setInterval(function(){
+		var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+		//when bird goes to the very top, then don't keep adding to the top
+		if(charachterTop>6){
+		charachter.style.top = (charachterTop-5)+ "px"
+		}
+		if(jumpCount>20){
+			clearInterval(jumpInterval);
+			jumping=0;
+			jumpCount=0;
+		}
+		jumpCount ++
+	},10);
+}
+ 
+
+
